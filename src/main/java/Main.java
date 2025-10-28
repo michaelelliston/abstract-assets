dacc0335764f9a7faeca70f1eb3cd5c3da3ed50c
michaelelliston <melliston@my.yearupunited.org>
2025-10-28 14:32:54 -0500

    finished code to loop through arraylist of assets in Main class, supposedly prints the proper contents when ran

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Asset> assets = new ArrayList<Asset>();
 
-        Vehicle vehicleOne = new Vehicle("My Car", "September 26, 2019", 10000, "Toyota", 5, 100762);
-        Vehicle vehicleTwo = new Vehicle("Dave's Truck", "January 2, 20011", 8000, "Ford", 14, 142973);
-        House houseOne = new House("My House", "March 12, 2014", 213000, "73 Real Str", 213, 245);
-        House houseTwo = new House("Your House", "December 1, 2016", 184221, "89 Fake Rd", 197, 232);
+        Vehicle vehicleOne = new Vehicle("my car", "September 26, 2019", 10000, "Toyota", 5, 100762);
+        Vehicle vehicleTwo = new Vehicle("Dave's truck", "January 2, 2011", 8000, "Ford", 14, 142973);
+        House houseOne = new House("my house", "March 12, 2014", 213000, "73 Real Str", 213, 245);
+        House houseTwo = new House("your house", "December 1, 2016", 184221, "89 Fake Rd", 197, 232);
 
         System.out.printf("The value of %s is: $%.2f\n", vehicleOne.getDescription(), vehicleOne.getValue());
 
@@ -17,7 +17,13 @@ public class Main {
         assets.add(houseTwo);
 
         for (Asset asset : assets) {
-            System.out.printf("This asset is %s, acquired %s, and is worth $%.2f\n", asset.getDescription(), asset.getDateAcquired(), asset.getValue());
+            if (asset instanceof House) {
+                House castedAsset = (House) asset;
+                System.out.printf("This asset is %s, acquired %s, and is worth $%.2f. It is located at %s.\n", castedAsset.getDescription(), castedAsset.getDateAcquired(), castedAsset.getValue(), castedAsset.getAddress());
+            } else if (asset instanceof Vehicle) {
+                Vehicle castedAsset = (Vehicle) asset;
+                System.out.printf("This asset is %s, acquired %s, and is worth $%.2f. It is a %s, and is %d years old.\n", castedAsset.getDescription(), castedAsset.getDateAcquired(), castedAsset.getValue(), castedAsset.getMakeModel(), castedAsset.getYear());
+            }
         }
     }
 }
